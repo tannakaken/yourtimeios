d7e9129486cb70d7f0f890e948d40a5cca4d2ce5
tannakaken <tannakaken@gmail.com>
2018-05-07 13:06:21 +0900

    Initial Commit

---
+++ YourTime/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  YourTime
+//
+//  Created by Kensaku Tanaka on 2018/05/07.
+//  Copyright © 2018年 Kensaku Tanaka. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
