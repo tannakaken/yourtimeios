c91af7ca4a1fe1d36b82105419142b301a25866c
tannakaken <tannakaken@gmail.com>
2018-06-29 15:03:07 +0900

    ページ遷移をスクローつに変更

--- YourTime/ModelController.swift
+++ YourTime/ModelController.swift
@@ -9,7 +9,6 @@
 import UIKit
 
 /*
- A controller object that manages a simple model -- a collection of month names.
  
  The controller serves as the data source for the page view controller; it therefore implements pageViewController:viewControllerBeforeViewController: and pageViewController:viewControllerAfterViewController:.
  It also implements a custom method, viewControllerAtIndex: which is useful in the implementation of the data source methods, and in the initial configuration of the application.
