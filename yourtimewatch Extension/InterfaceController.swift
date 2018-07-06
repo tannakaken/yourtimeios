e013920da992417c975843cc5bfa3919b93d15f2
tannakaken <tannakaken@gmail.com>
2018-07-06 11:16:42 +0900

    AppleWatchを付け加える

---
+++ yourtimewatch Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  yourtimewatch Extension
+//
+//  Created by Kensaku Tanaka on 2018/07/06.
+//  Copyright © 2018年 Kensaku Tanaka. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
