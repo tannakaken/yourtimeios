cf232089f78922c298a0894e9949e5d65ed629e2
tannakaken <tannakaken@gmail.com>
2018-07-09 22:26:07 +0900

    apple watchの時計の切り替えもできるようになった

--- yourtimewatch Extension/InterfaceController.swift
+++ yourtimewatch Extension/InterfaceController.swift
@@ -13,8 +13,33 @@ import WatchConnectivity
 
 class InterfaceController: WKInterfaceController, WCSessionDelegate {
     
-    var clocks: [Clock] = Clock.defaultClocks()
-    var index = 0
+    var clocks: [Clock] = Clock.defaultClocks() {
+        didSet {
+            if self.clocks.count == 0 {
+                self.clocks = Clock.defaultClocks()
+            }
+        }
+    }
+    var index = 0 {
+        didSet {
+            if self.index >= clocks.count {
+                self.index = 0
+            }
+            UserDefaults.standard.set(self.index, forKey: "index")
+        }
+    }
+    var isBlackBackground: Bool = UserDefaults.standard.bool(forKey: "isBlackBackground")
+    var backgroundColor: UIColor {
+        return isBlackBackground ? .black : .white
+    }
+    var foregroundColor: UIColor {
+        return isBlackBackground ? .white : .black
+    }
+    
+    var centerX: CGFloat = 0.0
+    var centerY: CGFloat = 0.0
+    var radius: CGFloat = 0.0
+    
     
     @IBOutlet var image: WKInterfaceImage!
     override func awake(withContext context: Any?) {
@@ -32,6 +57,7 @@ class InterfaceController: WKInterfaceController, WCSessionDelegate {
             let filePath = dir.appendingPathComponent("clocks.txt")
             self.clocks = self.parse(url: filePath)
         }
+        self.index = UserDefaults.standard.integer(forKey: "index")
     }
     
     func send(_ session: WCSession) {
@@ -43,31 +69,35 @@ class InterfaceController: WKInterfaceController, WCSessionDelegate {
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        centerX = self.contentFrame.width / 2
+        centerY = self.contentFrame.height / 2
+        radius = CGFloat.minimum(centerX, centerY)
         self.draw()
         self.animation()
     }
     
     func draw() {
-        let width = self.contentFrame.width
-        let height = self.contentFrame.height
-        let size = CGSize(width: width, height: height)
-        UIGraphicsBeginImageContext(size)
+        UIGraphicsBeginImageContext(self.contentFrame.size)
         if let context : CGContext = UIGraphicsGetCurrentContext() {
-            context.setFillColor(UIColor.white.cgColor)
-            context.fill(CGRect(x: 0, y: 0, width: width, height: height))
+            context.setFillColor(backgroundColor.cgColor)
+            context.fill(CGRect(x: 0,
+                                y: 0,
+                                width: self.contentFrame.width,
+                                height: self.contentFrame.height))
             let now = clocks[index].calc(time: Date().millisecond())
             let hourUnitAngle = Clock.unitAngle(totalNum: clocks[index].hours)
             let hourRadian = clocks[index].sig * now.hour * hourUnitAngle
-            let hourLength = self.contentFrame.midX * 0.3
+            let hourLength = radius * 0.3
             drawNeedle(width: CGFloat(3.0), length: hourLength, radian: hourRadian, context: context)
             let minuteUnitAngle = Clock.unitAngle(totalNum: clocks[index].minutes)
             let minuteRadian = clocks[index].sig * Double(now.minute) * minuteUnitAngle
-            let minuteLength = self.contentFrame.midX * 0.5
+            let minuteLength = radius * 0.5
             drawNeedle(width: CGFloat(1.5), length: minuteLength, radian: minuteRadian, context: context)
             let secondUnitAngle = Clock.unitAngle(totalNum: clocks[index].seconds)
             let secondRadian = clocks[index].sig * Double(now.second) * secondUnitAngle
-            let secondLength = self.contentFrame.midX * 0.7
+            let secondLength = radius * 0.8
             drawNeedle(width: CGFloat(0.8), length: secondLength, radian: secondRadian, context: context)
+            self.setTitle(clocks[index].name)
             drawDial()
             if let cgImage:CGImage = context.makeImage() {
                 let uiImage = UIImage(cgImage: cgImage)
@@ -81,31 +111,30 @@ class InterfaceController: WKInterfaceController, WCSessionDelegate {
         let fontSize = CGFloat(10)
         let attributes: [NSAttributedStringKey : AnyObject] = [
             .font: UIFont.systemFont(ofSize: fontSize),
-            .foregroundColor: UIColor.black
+            .foregroundColor: foregroundColor
         ]
-        let radius = self.contentFrame.midX * 0.8
+        let dialRadius = radius * 0.9
         let angle = clocks[index].sig * Clock.unitAngle(totalNum: clocks[index].hours)
         let range = clocks[index].dialFromOne ? 1...clocks[index].hours : 0...(clocks[index].hours-1)
         for i in range {
-            let x = self.contentFrame.midX + radius * CGFloat(sin(Double(i) * angle)) - fontSize/2
-            let y = self.contentFrame.midY - radius * CGFloat(cos(Double(i) * angle)) - fontSize/2
+            let x = centerX + dialRadius * CGFloat(sin(Double(i) * angle)) - fontSize/2
+            let y = centerY - dialRadius * CGFloat(cos(Double(i) * angle)) - fontSize/2
             NSAttributedString(string: "\(i)", attributes:attributes).draw(at: CGPoint(x:x,y:y))
         }
     }
     
     func drawNeedle(width: CGFloat, length: CGFloat, radian: Double, context: CGContext) {
-        let midX = self.contentFrame.midX
-        let midY = self.contentFrame.midY
-        context.move(to: CGPoint(x:midX,y:midY))
+        context.setStrokeColor(foregroundColor.cgColor)
+        context.move(to: CGPoint(x:centerX,y:centerY))
         context.setLineWidth(width)
         let dx = length * CGFloat(sin(radian))
         let dy = -length * CGFloat(cos(radian))
-        context.addLine(to: CGPoint(x: midX + dx, y: midY+dy))
+        context.addLine(to: CGPoint(x: centerX + dx, y: centerY+dy))
         context.strokePath()
     }
     
     func animation() {
-        self.animate(withDuration: 0.5, animations: {
+        self.animate(withDuration: 0.1, animations: {
             self.draw()
             self.animation()
         })
@@ -167,5 +196,24 @@ class InterfaceController: WKInterfaceController, WCSessionDelegate {
             print("cant parse file")
         }
         return result
+    }
+    
+    @IBAction func onSwipeRight(_ sender: Any) {
+        if self.index > 0 {
+            self.index -= 1
+        }
+        UserDefaults.standard.set(self.index, forKey: "index")
+    }
+    
+    @IBAction func onSwipeLeft(_ sender: Any) {
+        if self.index < self.clocks.count - 1 {
+            self.index += 1
+        }
+        UserDefaults.standard.set(self.index, forKey: "index")
+    }
+    
+    @IBAction func onDobleTap(_ sender: Any) {
+        isBlackBackground = !isBlackBackground
+        UserDefaults.standard.set(isBlackBackground, forKey: "isBlackBackground")
     }
 }
