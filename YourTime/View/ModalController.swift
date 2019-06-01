42982d5a33065c8b6b067445bf360831828f24a6
tanakakensaku <tannakaken@gmail.com>
2019-06-01 11:21:53 +0900

    Constraints系のwarningを全て消した

--- YourTime/View/ModalController.swift
+++ YourTime/View/ModalController.swift
@@ -51,17 +51,6 @@ class ModalController: UIViewController {
     @IBAction func close(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 // Helper function inserted by Swift 4.2 migrator.
