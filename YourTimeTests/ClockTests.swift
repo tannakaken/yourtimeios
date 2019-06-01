54dcbd2f85a2344b4dea2c93cfdbc7fd06ed0341
tanakakensaku <tannakaken@gmail.com>
2019-06-01 17:38:41 +0900

    テストを少しだけ書いた

--- YourTimeTests/ClockTests.swift
+++ YourTimeTests/ClockTests.swift
@@ -21,10 +21,36 @@ class ClockTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testDefaultClock() {
+        let clock = Clock.defaultClock()
+        
+        XCTAssertEqual(clock.name, NSLocalizedString("newtime", comment: ""))
+        XCTAssertEqual(clock.hours, 12)
+        XCTAssertEqual(clock.ampm, Ampm.ampm)
+        XCTAssertEqual(clock.minutes, 60)
+        XCTAssertEqual(clock.seconds, 60)
+        XCTAssertEqual(clock.dialFromOne, true)
+        XCTAssertEqual(clock.clockwise, true)
     }
+    
+    func testCustomizeClock() {
+        let clock = Clock(name: "decimal clock",
+                          ampm: .am,
+                          hours: 10,
+                          minutes: 100,
+                          seconds: 100,
+                          dialFromOne: false,
+                          clockwise:true)
+        
+        XCTAssertEqual(clock.name, "decimal clock")
+        XCTAssertEqual(clock.hours, 10)
+        XCTAssertEqual(clock.ampm, Ampm.am)
+        XCTAssertEqual(clock.minutes, 100)
+        XCTAssertEqual(clock.seconds, 100)
+        XCTAssertEqual(clock.dialFromOne, false)
+        XCTAssertEqual(clock.clockwise, true)
+    }
+    
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
